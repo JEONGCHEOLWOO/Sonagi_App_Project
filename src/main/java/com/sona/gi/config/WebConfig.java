a49990ef72f18e5e73744f0581b92edd6837d005
JEONG Cheolwoo <tonyprince1228@gmail.com>
2023-11-21 11:45:59 +0900

    commit change location

--- src/main/java/com/sona/gi/config/WebConfig.java
+++ src/main/java/com/sona/gi/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.sona.gi;
+package com.sona.gi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
