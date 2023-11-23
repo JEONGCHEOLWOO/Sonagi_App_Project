37359155aefe934f15f94bcaf978d281f54241e2
JEONG Cheolwoo <tonyprince1228@gmail.com>
2023-11-23 16:22:00 +0900

    update FoodDto
    update FoodDto

--- src/main/java/com/sona/gi/model/food/dto/FoodDto.java
+++ src/main/java/com/sona/gi/model/food/dto/FoodDto.java
@@ -1,14 +1,11 @@
 package com.sona.gi.model.food.dto;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class FoodDto {
 
     //기부할 음식 등록
 
+    private String id;
     //음식 이름
     private String foodName;
 
@@ -27,4 +24,69 @@ public class FoodDto {
     //가게 이름
     private String foodGiver;
 
+    private String foodImage;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getFoodAmount() {
+        return foodAmount;
+    }
+
+    public void setFoodAmount(String foodAmount) {
+        this.foodAmount = foodAmount;
+    }
+
+    public String getFoodPrice() {
+        return foodPrice;
+    }
+
+    public void setFoodPrice(String foodPrice) {
+        this.foodPrice = foodPrice;
+    }
+
+    public String getFoodTel() {
+        return foodTel;
+    }
+
+    public void setFoodTel(String foodTel) {
+        this.foodTel = foodTel;
+    }
+
+    public String getFoodAddress() {
+        return foodAddress;
+    }
+
+    public void setFoodAddress(String foodAddress) {
+        this.foodAddress = foodAddress;
+    }
+
+    public String getFoodGiver() {
+        return foodGiver;
+    }
+
+    public void setFoodGiver(String foodGiver) {
+        this.foodGiver = foodGiver;
+    }
+
+    public String getFoodImage() {
+        return foodImage;
+    }
+
+    public void setFoodImage(String foodImage) {
+        this.foodImage = foodImage;
+    }
 }
