76773a3ba2dcb8cf3495437db24d94549ae29f83
JEONG Cheolwoo <tonyprince1228@gmail.com>
2023-11-23 19:54:52 +0900

    Update FoodController.java

--- src/main/java/com/sona/gi/controller/food/FoodController.java
+++ src/main/java/com/sona/gi/controller/food/FoodController.java
@@ -1,13 +1,10 @@
 package com.sona.gi.controller.food;
 
-import com.sona.gi.model.donation.dto.DonationDto;
 import com.sona.gi.model.food.dto.FoodDto;
-import com.sona.gi.service.donation.DonationService;
 import com.sona.gi.service.food.FoodService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -33,10 +30,14 @@ public class FoodController {
     //    @RequestMapping(method = RequestMethod.POST, path = "/postMethod")
     // 아래랑 동일
     @GetMapping("/findAll")
-    public List<FoodDto> findAll(){
+    public HashMap<String,Object> findAll(){
+        HashMap<String,Object> mv = new HashMap<>();
         List<FoodDto> list = foodService.findAll();
-        return list;
+        mv.put("list",list);
+        return mv;
     }
+
+
 
     //검색 get, 수정 삭제 post
     @PostMapping("/modify")
