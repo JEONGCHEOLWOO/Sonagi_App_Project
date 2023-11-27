474ba61334b8cdde219182f552615f71f60101cb
JEONG Cheolwoo <tonyprince1228@gmail.com>
2023-11-27 21:18:48 +0900

    Update NotificationController.java

--- src/main/java/com/sona/gi/controller/notification/NotificationController.java
+++ src/main/java/com/sona/gi/controller/notification/NotificationController.java
@@ -21,64 +21,40 @@ public class NotificationController {
     @Autowired
     private MemberService memberService;
 
-    @PostMapping("/sendNotification")
-    public ResponseEntity<String> sendNotification(@RequestBody List<MemberDto> members) {
-        // Extracting addresses from the provided list of MemberDto objects
-        List<String> addresses = members.stream()
-                .map(MemberDto::getAddress)
-                .collect(Collectors.toList());
-
-        // DB에서 해당 주소들의 회원 정보 가져오기
+    @GetMapping("/sendNotification")
+    public ResponseEntity<String> sendNotification() {
+        // DB에서 모든 회원 정보 가져오기
         List<MemberDto> allMembers = memberService.findAll();
 
-        // 5km 이내에 있는 회원 필터링
-        List<MemberDto> nearbyMembers = filterNearbyMembers(allMembers, members);
-
-        // 필터링된 회원들의 푸시 알림 토큰 수집
-        List<String> tokens = nearbyMembers.stream()
+        // 모든 회원들의 푸시 알림 토큰 수집
+        List<String> tokens = allMembers.stream()
                 .map(MemberDto::getToken)
                 .collect(Collectors.toList());
 
         // 알림 메시지 생성 및 전송
         sendPushNotification(tokens);
 
-        return new ResponseEntity<>("Successfully sent message", HttpStatus.OK);
-    }
-
-    // 현재 주소와 5km 이내에 있는 회원 필터링
-    private List<MemberDto> filterNearbyMembers(List<MemberDto> allMembers, List<MemberDto> targetMembers) {
-        // 여기서는 주소가 일치하는 회원을 5km 이내에 있다고 가정합니다.
-        // 실제로는 주소를 좌표로 변환하여 거리를 계산하는 로직이 필요합니다.
-        List<String> targetAddresses = targetMembers.stream()
-                .map(MemberDto::getAddress)
-                .collect(Collectors.toList());
-
-        return allMembers.stream()
-                .filter(member -> targetAddresses.contains(member.getAddress()))
-                .collect(Collectors.toList());
+        return new ResponseEntity<>("Successfully sent message to all members", HttpStatus.OK);
     }
 
     // 푸시 알림 전송
     private void sendPushNotification(List<String> tokens) {
-        // 알림을 전송하는 코드 (이미 작성한 코드 활용)
         RestTemplate restTemplate = new RestTemplate();
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
-        // 주소를 포함한 알림 메시지 생성
-        Map<String, Object> body = new HashMap<>();
-        body.put("title", "음식 등록 알림");
-        body.put("body", "5km 이내 식당에서 음식이 등록되었습니다!");
-        body.put("sound", "default");
-        body.put("data", Collections.singletonMap("withSome", "data"));
-
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
-
         for (String token : tokens) {
+            Map<String, Object> notification = new HashMap<>();
+            notification.put("to", token);
+            notification.put("title", "음식 등록 알림");
+            notification.put("body", "5km 이내 식당에서 음식이 등록되었습니다!");
+            notification.put("sound", "default");
+            notification.put("data", Collections.singletonMap("withSome", "data"));
+
+            HttpEntity<Map<String, Object>> entity = new HttpEntity<>(notification, headers);
+
             try {
-                // 메세지 전송
                 ResponseEntity<String> response = restTemplate.exchange(
                         "https://exp.host/--/api/v2/push/send",
                         HttpMethod.POST,
@@ -86,13 +62,15 @@ public class NotificationController {
                         String.class);
 
                 if (response.getStatusCode() == HttpStatus.OK) {
-                    System.out.println("Successfully sent message to token " + token);
+                    System.out.println("Successfully sent message to member with token: " + token);
                 } else {
-                    System.err.println("Failed to send message to token " + token);
+                    System.err.println("Failed to send message to member with token: " + token);
                 }
             } catch (Exception e) {
-                System.err.println("Error sending message to token " + token + ": " + e.getMessage());
+                System.err
+                        .println("Error sending message to member with token: " + token + ", error: " + e.getMessage());
             }
         }
     }
+
 }
